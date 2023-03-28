41b0a215832163e7fc66f8ae6dc99eab30496f3d
lddtoan <lddtoan@outlook.com>
2023-03-28 14:36:09 +0700

    update: tests

--- __tests__/helper.test.ts
+++ __tests__/helper.test.ts
@@ -1,13 +1,19 @@
+import { config } from "dotenv";
 import { getContent } from "../shared/helper";
 
-test("get about page content", () => {
-  expect(getContent("about")).not.toEqual({ props: { content: [] } });
+config({ path: ".env.local" });
+
+test("get about page content", async () => {
+  const response = await getContent("about");
+  expect(response).not.toEqual({ props: { content: [] } });
 });
 
-test("get work page content", () => {
-  expect(getContent("work")).not.toEqual({ props: { content: [] } });
+test("get work page content", async () => {
+  const response = await getContent("work");
+  expect(response).not.toEqual({ props: { content: [] } });
 });
 
-test("get contact page content", () => {
-  expect(getContent("contact")).not.toEqual({ props: { content: [] } });
+test("get contact page content", async () => {
+  const response = await getContent("contact");
+  expect(response).not.toEqual({ props: { content: [] } });
 });
